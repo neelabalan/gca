d6d4faa97140d261273d7ae9ebfb58e18810f967
neelabalan <neelabalan.n@outlook.com>
2021-06-26 20:26:06 +0530

    changes done for functional code

--- gca/gca.py
+++ gca/gca.py
@@ -1,95 +1,89 @@
 import subprocess
 import requests
 import sys
-import click
+from typing import List, Dict 
 
-from rich.table import Column
+import click
+from rich import print
 from rich.progress import Progress, BarColumn, TimeElapsedColumn
 
 USER_API_URL = 'https://api.github.com/users/'
 ORG_API_URL = 'https://api.github.com/orgs/'
+DEVNULL = subprocess.DEVNULL
+
 bar_column =  BarColumn(bar_width=None, complete_style='blue')
+time_elapsed_column = TimeElapsedColumn()
 progress_console = "[progress.percentage]{task.percentage:>3.0f}%"
 
-def construct_url(user, org_type, api='repo'):
-    url_prefix = USER_API_URL + user if org_type == 'User' else ORG_API_URL + user 
+def construct_url(user: str, usertype: str, api: str='repo') -> str:
+    url_prefix = USER_API_URL + user if usertype == 'User' else ORG_API_URL + user 
     if api=='repo': 
         return url_prefix + '/repos?per_page=100&page={page}'
     else:
         return url_prefix + '/gists?per_page=100&page={page}'
 
-def fetch_response(url):
+
+def fetch_response(url: str) -> List[Dict]:
     '''fetch all repo response by iterating the pages'''
     responses = list()
-    page = 0
+    page = 1 # starting with page 1
     while True:
-        response = requests.get(url.format(page=page))
+        response = requests.get(url.format(page=page)).json()
         page += 1
-        if response:
-            responses += response
-        else:
-            break
+        if not response:
+            break # break from loop when no response
+        responses += response
     return responses
 
 
-def get_clone_urls(response, url_type='http') -> dict:
+def get_clone_urls(response: Dict, ssh: bool) -> Dict:
     clone_urls = list()
-    if response.get('type') == 'repo':
-        clone_urls = {
-            response.get('name'): response.get('clone_url')  for repo in response.get('gca.repositories')
-        }
-    if 'gist' in response.get('type'):
-        clone_urls.update({
-            gist.get('id'): gist.get('git_pull_url') for gist in response.get('gca.gists')
-        }) 
+    repo_clone_url = 'ssh_url' if ssh else 'clone_url'
+    if response.get('repo'):
+        clone_urls = { repo.get('name'): repo.get(repo_clone_url) for repo in response.get('repo') }
+    if response.get('gist'):
+        clone_urls.update({ gist.get('id'): gist.get('git_pull_url') for gist in response.get('gist') })
     return clone_urls
 
 
-def get_user_response(username):
+def get_user_response(username: str) -> Dict:
     '''return user details'''
     try:
-        response = requests.get(''.join([USER_API_URL, username]))
+        response = requests.get(USER_API_URL + username)
         response.raise_for_status()
     except requests.exceptions.RequestException as err:
         print('Could not get proper response')
-        sys.exit(err)
+        print('[red]{}[/red]'.format(response.json()))
+        sys.exit()
     finally:
         return response.json()
 
 
 def execute_cloning(url_list: dict):
     if not url_list:
-        raise RuntimeError('No URL provided for cloning')
-    with Progress(TimeElapsedColumn(), columns=bar_column, console=progress_console, expand=True) as progress:
+        raise RuntimeError('No URL provided')
+    with Progress(bar_column, progress_console, time_elapsed_column, expand=True) as progress:
         task = progress.add_task("[blue]Cloning...", total=len(url_list))
-        for count, repo in enumerate(url_list, start=1):
+        for name, link in url_list.items():
             progress.update(task, advance=1)
-            subprocess.run(
-                args=['git', 'clone', repo[1]],
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-            )
-            progress.print('[green]✓[/green] ' + repo[0])
+            subprocess.run(args=['git', 'clone', link], stdout=DEVNULL, stderr=DEVNULL)
+            progress.print('[green]✓[/green] ' + name)
 
 
-def run(user, ssh, gist):
+def run(user: str, ssh: bool, gist: bool):
     response = get_user_response(user)
-    total_repo, total_gist, usertype = (
-        response.get('public_repos'),
-        response.get('public_gists'),
-        response.get('type')
-    )
+    usertype = response.get('type')
     gh_response = dict()
     gh_response['repo'] = fetch_response(url = construct_url(user, usertype, 'repo'))
     if gist:
         gh_response['gist'] = fetch_response(url = construct_url(user, usertype, 'gist'))
-    clone_urls = get_clone_urls(gh_response)
+    clone_urls = get_clone_urls(gh_response, ssh)
     execute_cloning(clone_urls)
 
 
-@click.command
+@click.command()
 @click.option('--user', required=True, type=str)
-@click.option('--ssh', type=str)
+@click.option('--ssh', type=bool)
 @click.option('--gist', type=bool)
 def main(user, ssh, gist):
     run(user, ssh, gist) 
