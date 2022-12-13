7efa79713da8c72778025dabc42deeb7bb295662
neelabalan <neelabalan.n@outlook.com>
2022-12-13 11:36:50 +0530

    code formatted

--- setup.py
+++ setup.py
@@ -10,24 +10,19 @@ with open(join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name             = "ghub_clone",
-    version          = "0.3.0",
-    url              = "https://github.com/neelabalan/gca",
-    long_description = long_description, 
+    name="ghub_clone",
+    version="0.3.0",
+    url="https://github.com/neelabalan/gca",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    author           = "neelabalan",
-    author_email     = "neelabalan.n@gmail.com",
-    python_requires  = ">=3.8",
-    license          = "MIT",
-    install_requires = [
-        "requests>=2.20.0",
-        "rich>=9.12.3"
-    ],
-    keywords         = "git clone github",
-    packages         = find_packages(exclude=["*.test", "*.test.*", "test.*", "test"]), 
-    scripts          =['gca.py'],
-    entry_points     = {
-        "console_scripts": [ "gca = gca:main" ]
-    },
-    setup_requires   = ["wheel"],
+    author="neelabalan",
+    author_email="neelabalan.n@gmail.com",
+    python_requires=">=3.8",
+    license="MIT",
+    install_requires=["requests>=2.20.0", "rich>=9.12.3"],
+    keywords="git clone github",
+    packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test"]),
+    scripts=["gca.py"],
+    entry_points={"console_scripts": ["gca = gca:main"]},
+    setup_requires=["wheel"],
 )
--- test_gca.py
+++ test_gca.py
@@ -1,8 +1,15 @@
 import unittest
 from unittest import mock
-from gca import construct_url, fetch_response, get_clone_urls, get_user_response, execute_cloning
+from gca import (
+    construct_url,
+    fetch_response,
+    get_clone_urls,
+    get_user_response,
+    execute_cloning,
+)
 
 DEVNULL = -3
+
 
 class MockResponse:
     def __init__(self, json_data, status_code):
@@ -12,78 +19,87 @@ class MockResponse:
     def json(self):
         return self.json_data
 
+
 class TestGca(unittest.TestCase):
     def test_construct_url(self):
         self.assertEqual(
-            'https://api.github.com/users/testuser/repos?per_page=100&page={page}', 
-            construct_url('testuser', 'User', 'repo')
+            "https://api.github.com/users/testuser/repos?per_page=100&page={page}",
+            construct_url("testuser", "User", "repo"),
         )
         self.assertEqual(
-            'https://api.github.com/users/testuser/gists?per_page=100&page={page}', 
-            construct_url('testuser', 'User', 'gist')
+            "https://api.github.com/users/testuser/gists?per_page=100&page={page}",
+            construct_url("testuser", "User", "gist"),
         )
         self.assertEqual(
-            'https://api.github.com/orgs/testuser/repos?per_page=100&page={page}', 
-            construct_url('testuser', 'Orgs', 'repo')
+            "https://api.github.com/orgs/testuser/repos?per_page=100&page={page}",
+            construct_url("testuser", "Orgs", "repo"),
         )
 
-    @mock.patch('gca.requests')
+    @mock.patch("gca.requests")
     def test_fetch_response(self, mock_requests):
         mock_requests.get.side_effect = [
             MockResponse(
-                json_data=[{'clone_url': 'https://gist.github.com/123456789.git', 'name': 'testrepo'}],
-                status_code=200
+                json_data=[
+                    {
+                        "clone_url": "https://gist.github.com/123456789.git",
+                        "name": "testrepo",
+                    }
+                ],
+                status_code=200,
             ),
-            MockResponse(
-                json_data=[],
-                status_code=200
-            )
+            MockResponse(json_data=[], status_code=200),
         ]
         self.assertEqual(
-            [{'clone_url': 'https://gist.github.com/123456789.git', 'name': 'testrepo'}],
-            fetch_response('https://api.github.com/orgs/testuser/repos?per_page=100&page={page}') 
+            [
+                {
+                    "clone_url": "https://gist.github.com/123456789.git",
+                    "name": "testrepo",
+                }
+            ],
+            fetch_response(
+                "https://api.github.com/orgs/testuser/repos?per_page=100&page={page}"
+            ),
         )
-
 
     def test_get_clone_urls(self):
         response = {
-            'repo': [{ 
-                'clone_url': 'https://github.com:user1/repo1.git',  
-                'ssh_url': 'git@github.com:user2/repo2',
-                'name': 'project1'
-            }]
+            "repo": [
+                {
+                    "clone_url": "https://github.com:user1/repo1.git",
+                    "ssh_url": "git@github.com:user2/repo2",
+                    "name": "project1",
+                }
+            ]
         }
         self.assertEqual(
-            { 'project1': 'https://github.com:user1/repo1.git'},
-            get_clone_urls(response, ssh=False)
+            {"project1": "https://github.com:user1/repo1.git"},
+            get_clone_urls(response, ssh=False),
         )
         self.assertEqual(
-            { 'project1': 'git@github.com:user2/repo2'},
-            get_clone_urls(response, ssh=True)
+            {"project1": "git@github.com:user2/repo2"},
+            get_clone_urls(response, ssh=True),
         )
 
-
-    @mock.patch('gca.requests')
+    @mock.patch("gca.requests")
     def test_get_user_response(self, mock_requests):
-        mock_requests.get.return_value = MockResponse(json_data={'user': 'rick'}, status_code=404)
-        self.assertEqual(
-            get_user_response('rick'),
-            {'user': 'rick'}
+        mock_requests.get.return_value = MockResponse(
+            json_data={"user": "rick"}, status_code=404
         )
+        self.assertEqual(get_user_response("rick"), {"user": "rick"})
 
-
-    @mock.patch('gca.subprocess')
+    @mock.patch("gca.subprocess")
     def test_execute_cloning(self, mock_subp):
         url_map = {
-            'firstrepo': 'git@github.com:user1/repo1',
-            'secondrepo': 'git@github.com:user2/repo2'
+            "firstrepo": "git@github.com:user1/repo1",
+            "secondrepo": "git@github.com:user2/repo2",
         }
         execute_cloning(url_map)
         mock_subp.run.assert_any_call(
-            args=['git', 'clone', 'git@github.com:user1/repo1'],
+            args=["git", "clone", "git@github.com:user1/repo1"],
             stdout=DEVNULL,
             stderr=DEVNULL,
         )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
